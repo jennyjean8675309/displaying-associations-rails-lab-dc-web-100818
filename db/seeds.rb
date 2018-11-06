beb0d0e920aef3f721c091ea66cadb8013e2a840
jennyjean <jennyjean8675309@yahoo.com>
2018-11-06 14:23:39 -0500

    Created models and views with associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#Artists
+Artist.destroy_all
+Song.destroy_all
+
+bono = Artist.create(name: "Bono")
+adele = Artist.create(name: "Adele")
+tom_petty = Artist.create(name: "Tom Petty")
+
+#Songs
+desire = Song.create(title: "Desire", artist_id: bono.id)
+hello = Song.create(title: "Hello", artist_id: adele.id)
+learning_to_fly = Song.create(title: "Learning to Fly", artist_id: tom_petty.id)
+in_the_name_of_love = Song.create(title: "In the Name of Love", artist_id: bono.id)
